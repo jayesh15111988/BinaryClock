d3764ed6bf9ae4d7ff181032dbf4263b3ade042e
Jayesh Kawli <j.kawli@gmail.com>
2015-01-11 22:16:38 -0500

    Added binary clock support. Optimization pending

--- js/timeLogic.js
+++ js/timeLogic.js
@@ -2,6 +2,9 @@
  * Created by jayeshkawli on 1/11/15.
  */
 //Logic for Colored Clock
+
+var oldHoursFirstPart, oldHoursSecondPart, oldMinutesFirstPart, oldMinutesSecondPart, oldSecondsFirstPart, oldSecondsSecondPart;
+
 function updateColorClock() {
     var currentDate = new Date();
     var dateOnly = currentDate.today();
@@ -11,8 +14,85 @@ function updateColorClock() {
     var currentColorCode = currentDate.currentTimeColor();
     $('#color-code').text(currentColorCode);
     $('#color-clock').css("background-color", currentColorCode);
+
+    var splitTimeString = getIndividualTimeParameters(timeOnly);
+    for (var i = 0, totalSplitStringLength = splitTimeString.length; i < totalSplitStringLength; i++) {
+        $( "#b" + splitTimeString[i]).css("background-color", "black");
+    }
+}
+function getIndividualTimeParameters(timeString) {
+    var timeStringArrayRepresentation = timeString.split(":");
+    var setBitsHolder = [];
+
+    var hoursFirstPart =  parseInt(timeStringArrayRepresentation[0]/10);
+    var hoursSecondPart = timeStringArrayRepresentation[0]%10;
+
+    //Execute following function only if hour has changed
+    if(hoursFirstPart != oldHoursFirstPart || hoursSecondPart != oldHoursSecondPart) {
+        resetAllBullets([0,9]);
+        setBitsHolder = setBitsHolder.concat(getSetBits(hoursFirstPart, 0));
+        setBitsHolder = setBitsHolder.concat(getSetBits(hoursSecondPart, 1));
+        oldHoursFirstPart = hoursFirstPart;
+        oldHoursSecondPart =  hoursSecondPart;
+    }
+
+
+    var minutesFirstPart = parseInt(timeStringArrayRepresentation[1]/10);
+    var minutesSecondPart = timeStringArrayRepresentation[1]%10;
+
+    //Execute following function only if hour has changed
+    if(minutesFirstPart != oldMinutesFirstPart || minutesSecondPart != oldMinutesSecondPart) {
+        resetAllBullets([9,17]);
+        setBitsHolder = setBitsHolder.concat(getSetBits(minutesFirstPart, 2));
+        setBitsHolder = setBitsHolder.concat(getSetBits(minutesSecondPart, 3));
+        oldMinutesFirstPart = minutesFirstPart;
+        oldMinutesSecondPart = minutesSecondPart;
+    }
+
+    var secondsFirstPart = parseInt(timeStringArrayRepresentation[2]/10);
+    var secondsSecondPart = timeStringArrayRepresentation[2]%10;
+
+    //Execute following function only if hour has changed
+    if(secondsFirstPart != oldSecondsFirstPart || secondsSecondPart != oldSecondsSecondPart) {
+        resetAllBullets([17,24]);
+        setBitsHolder = setBitsHolder.concat(getSetBits(secondsFirstPart, 4));
+        setBitsHolder = setBitsHolder.concat(getSetBits(secondsSecondPart, 5));
+        oldSecondsFirstPart = secondsFirstPart;
+        oldSecondsSecondPart = secondsSecondPart;
+    }
+    return setBitsHolder;
 }
 
+function getSetBits(inputDecimalNumber, blockNumber) {
+    var decimalRepresentation = inputDecimalNumber.toString(2);
+
+    if (decimalRepresentation.length < 4) {
+        decimalRepresentation = Array(4 - decimalRepresentation.length + 1).join("0") + decimalRepresentation;
+    }
+    var decimalArrayRepresentation = decimalRepresentation.split("");
+
+    var setIndexCollection = [];
+    for(var i = 0; i < 4; i++) {
+        if(decimalArrayRepresentation[i] == "1") {
+            setIndexCollection.push(i+ (blockNumber *4));
+        }
+    }
+    return setIndexCollection;
+}
+
+function resetAllBullets(range) {
+        //We have three clock types - binary, base 4 and octal
+        var clockBases = ['b','f','o'];
+        for (var baseIndex = 0, baseSize = clockBases.length; baseIndex < baseSize; baseIndex++ ) {
+            for(var bulletIndex = range[0]; bulletIndex < range[1]; bulletIndex++ ) {
+                $(document).ready(function() {
+                    $( "#"+clockBases[baseIndex]+bulletIndex).css("background-color", "white");
+                });
+            }
+        }
+}
+
+resetAllBullets(0, 24);
 setInterval(updateColorClock, 1000);
 
 function getDateParameters() {
