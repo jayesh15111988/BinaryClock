2a4fda1f9a325bcbb08a92d95375dc9c021c214d
Jayesh Kawli <j.kawli@gmail.com>
2015-01-17 22:59:26 -0500

    Added support to show periodic elements data in the clock form

--- js/periodicElementsConfigurator.js
+++ js/periodicElementsConfigurator.js
@@ -5,24 +5,40 @@ var oldElementSequenceNumber = -1;
 var newElementSequenceNumber = 0;
 var inputRangeLower = 0, inputRangeUpper = 720;
 var outputRangeLower = 0, outputRangeUpper = 117;
+var nextUpdatedSecondsValue = -1;
+var didUpdateValueForFirstTime = false;
+var elementUpdateTimeNormalizingFactor = 6.14;
+
 function sendUpdateWithCurrentNumberOfSeconds(numberOfSecondsSoFar) {
     var numberOfMinutes = numberOfSecondsSoFar / 60;
     var scaledNumberOfMinutes = numberOfMinutes % 721;
 
     newElementSequenceNumber  = parseInt((scaledNumberOfMinutes-inputRangeLower)/(inputRangeUpper-inputRangeLower) * (outputRangeUpper-outputRangeLower) + outputRangeLower);
-    console.log("Element sequence number " + newElementSequenceNumber);
+
 
     if(newElementSequenceNumber != oldElementSequenceNumber) {
         var currentElementName = allElementsCollectionWithNames[newElementSequenceNumber];
         var currentElementDetails = elementsData[currentElementName]; //This is dictionary
-        configureCurrentViewWithElementDetails(currentElementDetails);
+        configureCurrentViewWithElementDetails(currentElementDetails, currentElementName);
         oldElementSequenceNumber = newElementSequenceNumber;
+        console.log("Current value of element update " + numberOfMinutes);
+        //We came here for the first time right
+
+        if(didUpdateValueForFirstTime == true) {
+            nextUpdatedSecondsValue = parseInt(elementUpdateTimeNormalizingFactor*60);
+        }
+
+        didUpdateValueForFirstTime = true;
+    }
+
+    if(nextUpdatedSecondsValue != -1) {
+        $("#next-time-till-update").text("Seconds till next update "+ nextUpdatedSecondsValue--);
     }
 }
 
-function configureCurrentViewWithElementDetails(currentElementDetails) {
+function configureCurrentViewWithElementDetails(currentElementDetails, elementName) {
     $("#element-name").text(currentElementDetails.symbol + " (" + currentElementDetails.atomic_number +")");
-    $("#atomic-number").text(currentElementDetails.atomic_number);
+    $("#name").text(elementName);
     $("#atomic-weight").text(currentElementDetails.atomic_weight);
     $("#density").text(currentElementDetails['density g/cm']);
     $("#melting-point").text(currentElementDetails['melting_point K']);
