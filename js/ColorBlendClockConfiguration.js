4edfb8d2a155f00981c36ed8c1e45fca1f4baad6
Jayesh Kawli <j.kawli@gmail.com>
2015-01-18 21:03:43 -0500

    Improved layout in terms of size and position

--- js/ColorBlendClockConfiguration.js
+++ js/ColorBlendClockConfiguration.js
@@ -8,7 +8,7 @@ var canvas = document.getElementById('color-gradient-canvas');
 var winMin = Math.min(window.innerWidth,window.innerHeight);
 
 canvas.width = winMin;
-canvas.height = winMin*0.70;
+canvas.height = winMin*0.65;
 
 var ctx = canvas.getContext('2d');
 
@@ -21,25 +21,37 @@ var ctx = canvas.getContext('2d');
 ctx.globalCompositeOperation = 'screen';
 
 //Create big circle centered on the screen
+ctx.strokeStyle = 'rgb(255,255,255)';
+ctx.beginPath();
+ctx.arc(canvas.width/2,(canvas.height/2) + 5,(canvas.height/2)-10, 0, Math.PI*2, true);
+ctx.lineWidth = 5;
+ctx.closePath();
+ctx.stroke();
 
+//Create a dot at the center of circle
+ctx.fillStyle = 'rgb(255,255,255)';
+ctx.beginPath();
+ctx.arc(canvas.width/2,canvas.height/2,5, 0, Math.PI*2, true);
+ctx.closePath();
+ctx.fill();
 
 //red - Hour Hand
 ctx.fillStyle = 'rgb(255,0,0)';
 ctx.beginPath();
-ctx.arc(100, 100, 15, 0, Math.PI*2, true);
+ctx.arc(100, 100, 25, 0, Math.PI*2, true);
 ctx.closePath();
 ctx.fill();
 
 //green - Minute Hand
 ctx.fillStyle = 'rgb(0,255,0)';
 ctx.beginPath();
-ctx.arc(100, 150, 30, 0, Math.PI*2, true);
+ctx.arc(100, 150, 50, 0, Math.PI*2, true);
 ctx.closePath();
 ctx.fill();
 
 //blue - Second Hand
 ctx.fillStyle = 'rgb(0,0,255)';
 ctx.beginPath();
-ctx.arc(100, 200, 45, 0, Math.PI*2, true);
+ctx.arc(100, 200, 75, 0, Math.PI*2, true);
 ctx.closePath();
 ctx.fill();
