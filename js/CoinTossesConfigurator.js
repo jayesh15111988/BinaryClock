0dfbbce0f0ddfe5d1c1654084546f28cf843942f
Jayesh Kawli <j.kawli@gmail.com>
2015-01-18 19:38:12 -0500

    Added support to change image based on the majority side

--- js/CoinTossesConfigurator.js
+++ js/CoinTossesConfigurator.js
@@ -3,6 +3,12 @@
  */
 var numberOfFlips = 0;
 var numberOfHeads = 0, numberOfTails = 0;
+var majorityCoinEnum = {
+    HEAD : 0,
+    TAIL: 1
+};
+var oldMajority = majorityCoinEnum.HEAD;
+var newMajority = majorityCoinEnum.TAIL;
 
 function updateCoinTosses() {
     console.log("Updated..");
@@ -18,9 +24,26 @@ function updateCoinTosses() {
     var percentageHeads = parseFloat((numberOfHeads/numberOfFlips)*100).toFixed(2);
     var percentageTails = parseFloat(100.0 - percentageHeads).toFixed(2);
 
+    newMajority = (percentageHeads > percentageTails) ? majorityCoinEnum.HEAD : majorityCoinEnum.TAIL;
+    updateCoinImageIfNecessary();
+
     $("#number-of-heads").html(" Heads <br/>"+ numberOfHeads + "<br/>" + percentageHeads + "%");
     $("#number-of-tails").html(" Tails <br/>"+ numberOfTails + "<br/>" + percentageTails + "%");
 
+}
+
+function updateCoinImageIfNecessary() {
+    if(newMajority != oldMajority) {
+        if(newMajority == majorityCoinEnum.HEAD) {
+            //Change image source to head image
+            $('#coin-side-image').attr('src', 'images/coin-head-transparent.gif');
+        }
+        else {
+            //Change image source to tail image
+            $('#coin-side-image').attr('src', 'images/coin-tail-transparent.gif');
+        }
+        oldMajority = newMajority;
+    }
 }
 
 function getRandomInt(min, max) {
